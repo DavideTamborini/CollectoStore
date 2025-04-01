321637a94b2ddceb7b2a0565250f2208cac78647
davidetamborini <tambo1997@gmail.com>
2025-04-02 00:04:51 +0200

    First commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/collecto/store/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.collecto_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:collecto_store/app/app.dart';
+import 'package:collecto_store/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
