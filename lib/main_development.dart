783fc0c21e29ff486cce6d2fb9905b8cfd3c9742
davidetamborini <tambo1997@gmail.com>
2025-04-02 01:03:11 +0200

    Exported Dio library

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,12 @@
 import 'package:collecto_store/app/app.dart';
 import 'package:collecto_store/bootstrap.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final fakeApiClient = Dio();
+
   bootstrap(() => const App());
 }
--- packages/collecto_data/lib/collecto_data.dart
+++ packages/collecto_data/lib/collecto_data.dart
@@ -1,6 +1,9 @@
 /// A Very Good Project created by Very Good CLI.
 library;
 
+/// external library export
+export 'package:dio/dio.dart';
+
 export 'src/exceptions/exceptions.dart';
 export 'src/interfaces/interfaces.dart';
 export 'src/managers/managers.dart';
